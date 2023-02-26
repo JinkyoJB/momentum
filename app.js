39efc6a3957a258d94929b5fb3a39d02253599ec
jinkyo chung <85150616+JinkyoJB@users.noreply.github.com>
2023-02-27 07:56:07 +0900

    2.5class

---
+++ app.js
@@ -0,0 +1,72 @@
+// alert("hi");
+
+
+// console.log(545454);
+
+// console.log("lalalala");
+
+// console.log(5+2);
+
+// console.log(5*2);
+
+// console.log(5/2);
+
+// const a = 5;
+// const b = 2;
+// const myName = "nico";
+
+// console.log(a+b);
+
+// console.log(a*b);
+
+// console.log(a/b);
+
+// console.log("hello " + myName);
+
+//// 2.3 const and let
+// const a = 5;
+// const b = 2;
+// let myName = "nico";
+
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log("hello " + myName);
+
+// myName= "nicolas";
+
+// console.log("your new name is " + myName);
+
+//// // 2.4 booleans
+// const amIFat = true;
+// const amIThin = false;
+// const value = null;
+// let something;
+
+// console.log(something);
+
+////2.5 Arrays
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sat";
+// const sun = "sun";
+
+// const daysOfWeek = [mon , tue , wed , thu , fri , sat , sun];
+
+// const nonsense = [1, 2, "hello", false, null, true, undefined, "nico"];
+
+// console.log(daysOfWeek);
+// console.log(daysOfWeek);
+
+const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+
+
+//get item from array
+console.log(daysOfWeek[5]);
+
+//add one more day to the array
+daysOfWeek.push("sun")
+console.log(daysOfWeek);
