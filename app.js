591c8d97cee672e5f8c4319cb197cf651168ccac
jinkyo chung <85150616+JinkyoJB@users.noreply.github.com>
2023-02-27 09:35:05 +0900

    2.6 class

--- app.js
+++ app.js
@@ -61,12 +61,37 @@
 // console.log(daysOfWeek);
 // console.log(daysOfWeek);
 
-const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+// const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
 
 
-//get item from array
-console.log(daysOfWeek[5]);
+// //get item from array
+// console.log(daysOfWeek[5]);
 
-//add one more day to the array
-daysOfWeek.push("sun")
-console.log(daysOfWeek);
+// //add one more day to the array
+// daysOfWeek.push("sun")
+// console.log(daysOfWeek);
+
+////2.6 Objects
+// //방법 1
+// const playerName = "nico";
+// const playerPoints = "121212";
+// const playerHansome = false;
+// const playerFat = "little bit";
+
+// //방법2
+// const player = ["nico", 1212, false, "little bit"];
+
+// //방법3
+const player = {
+    name: "nico",
+    points: 10,
+    fat: true
+};
+
+console.log(player);
+console.log(player.name);
+
+player.fat = false;
+player.lastName = "potato";
+player.points = player.points + 15;
+console.log(player);
